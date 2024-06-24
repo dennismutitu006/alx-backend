9c1188b20ffd1b03f7b0546603f8536c8995653f
Dennis <dennismutitu06@gmail.com>
2024-06-24 10:36:04 +0000

    hypermedia pagination before testing it

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+'''
+function that takes 2 int args and returns a tuple of size 2: a start indx
+endindx corresponding to the range of indexes to return in a list for those
+particular pagination parameters.
+Page numbers are 1-indexed, i.e. the first page is page 1.
+'''
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''the get page function'''
+        assert type(page_size) is int and type(page) is int
+        assert page > 0
+        assert page_size > 0
+        self.dataset()
+        i = index_range(page, page_size)
+        if i[0] >= len(self.__dataset):
+            return []
+        else:
+            return self.__dataset[i[0]:i[1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """takes same args as get_page and rtns key value dict"""
+        dataset_size = len(self.dataset())
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(dataset_size / page_size)
+
+        d = {
+                "page": page,
+                "page_size": page_size if page < total_pages else 0,
+                "data": data,
+                "next_page": page + 1 if page + 1 < total_pages else None,
+                "prev_page": page - 1 if page - 1 > 0 else None,
+                "total_pages": total_pages
+                }
+        return d
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    '''
+    Parameters:
+        page: the page number.
+        page_size: the number of characters for one page.
+
+    Returns:
+        a tuple with start index and end index.
+    '''
+
+    start_ind = (page - 1) * page_size
+    end_ind = start_ind + page_size
+
+    return (start_ind, end_ind)
