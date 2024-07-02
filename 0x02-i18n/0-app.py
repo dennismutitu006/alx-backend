24fccc0f3dd5229616e8cf6284a6feb6efd276ee
dennismutitu <dennismutitu06@gmail.com>
2024-07-02 10:01:21 -0700

    a basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+basic flask app.
+'''
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """route for the index page"""
+    return render_template('index.html')
+
+
+if __name__ = '__main__':
+    app.run(debug=True)
