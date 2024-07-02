fe82be1999cbeacdd7fb52bf2803af38d3a9c85c
dennismutitu <dennismutitu06@gmail.com>
2024-07-02 10:16:42 -0700

    flask babel configuration

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 '''
-basic flask app.
+basic flask app with i18n support using Flask-Babel.
 '''
 from flask import Flask, render_template
+from flask_babel import Babel
 
 
 app = Flask(__name__)
+
+
+class Config:
+    '''configuration class for setting languages and default locale'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/')
@@ -14,5 +26,5 @@ def index():
     return render_template('0-index.html')
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
